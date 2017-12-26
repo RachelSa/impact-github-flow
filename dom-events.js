88e9815c021bceb19b5cf0a1b94c89872fc16b77
github username <rachel.salois@gmail.com>
2017-12-26 12:06:38 -0500

    fetch requests

--- dom-events.js
+++ dom-events.js
@@ -3,11 +3,27 @@ const astrosEl = document.getElementById("astros")
 
 window.onload = () => {
   greetingEl.innerText = "oh, hey there!"
-  renderAstros()
+  getAstros()
 }
 
-renderAstros = () => {
-  const astros = peopleInSpace["people"]
+// getAstros = () => {
+//   fetch('http://api.open-notify.org/astros.json')
+//   .then(r => r.json())
+//   .then(astros => renderAstros(astros))
+// }
+
+async function getAstros() {
+    try {
+      let response = await fetch("http://api.open-notify.org/astros.json")
+      let astros = await response.json()
+      renderAstros(astros)
+    } catch (e) {
+      console.log(e)
+    }
+}
+
+renderAstros = (data) => {
+  const astros = data["people"]
   let peopleEls = astros.map((a) => {
     let name = a["name"]
     return `<li>${name}</li>`
