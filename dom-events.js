371897430a3d618c43f4925a54efa7e8c5f02b04
github username <rachel.salois@gmail.com>
2018-01-01 10:30:32 -0500

    added form practice

--- dom-events.js
+++ dom-events.js
@@ -1,19 +1,19 @@
 const greetingEl = document.getElementById("greeting")
 const astrosEl = document.getElementById("astros")
 
-window.onload = () => {
-  greetingEl.innerText = "oh, hey there!"
-  renderAstros()
-}
-
-renderAstros = () => {
-  const astros = peopleInSpace["people"]
-  let peopleEls = astros.map((a) => {
-    let name = a["name"]
-    return `<li>${name}</li>`
-  })
-  astrosEl.innerHTML = `<ul>${peopleEls.join("")}</ul>`
-}
+// window.onload = () => {
+//   greetingEl.innerText = "oh, hey there!"
+//   renderAstros()
+// }
+//
+// renderAstros = () => {
+//   const astros = peopleInSpace["people"]
+//   let peopleEls = astros.map((a) => {
+//     let name = a["name"]
+//     return `<li>${name}</li>`
+//   })
+//   astrosEl.innerHTML = `<ul>${peopleEls.join("")}</ul>`
+// }
 
  // create a function that alerts "these are all the astronauts!" when the title element is clicked.
 
@@ -24,3 +24,9 @@ renderAstros = () => {
 // create a text input element. When a user clicks on the text field to input, log the input element to console.
 
 // when a user clicks away from the input, log ("bye") to the console.
+
+// wrap the input element with a form element.
+
+// when the form is submitted, render the client's inputted text to the DOM
+
+// HINT: look into event.preventDefault()
