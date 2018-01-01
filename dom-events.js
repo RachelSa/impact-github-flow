698b0fc36ca93b98eabdaa587e4f20faa64ae644
github username <rachel.salois@gmail.com>
2018-01-01 09:47:42 -0500

    assignment added

---
+++ dom-events.js
@@ -0,0 +1,26 @@
+const greetingEl = document.getElementById("greeting")
+const astrosEl = document.getElementById("astros")
+
+window.onload = () => {
+  greetingEl.innerText = "oh, hey there!"
+  renderAstros()
+}
+
+renderAstros = () => {
+  const astros = peopleInSpace["people"]
+  let peopleEls = astros.map((a) => {
+    let name = a["name"]
+    return `<li>${name}</li>`
+  })
+  astrosEl.innerHTML = `<ul>${peopleEls.join("")}</ul>`
+}
+
+ // create a function that alerts "these are all the astronauts!" when the title element is clicked.
+
+ // log to the console the client's x coordinate of their mouse as they move it around the window.
+
+// make an element that displays the most recent key pressed in the DOM
+
+// create a text input element. When a user clicks on the text field to input, log the input element to console.
+
+// when a user clicks away from the input, log ("bye") to the console.
