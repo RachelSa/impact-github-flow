0ce99e79d9bad7ead487b86103f0d444e2001e45
github username <rachel.salois@gmail.com>
2018-01-02 18:50:28 -0500

    moved questions

--- dom-events.js
+++ dom-events.js
@@ -17,3 +17,19 @@ renderAstros = () => {
 }
 
  // create a function that alerts "these are all the astronauts!" when the title element is clicked.
+
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+// log to the console the client's x coordinate of their mouse as they move it around the window.
+
+// make an element that displays the most recent key pressed in the DOM
+
+// create a text input element. When a user clicks on the text field to input, log the input element to console.
+
+// when a user clicks away from the input, log ("bye") to the console.
+
+// wrap the input element with a form element.
+
+// when the form is submitted, render the client's inputted text to the DOM
+
+// HINT: look into event.preventDefault()
