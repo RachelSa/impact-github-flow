049a892221ee6a5fea76ef4ff079fe483c7c543c
rohitmusti <rohit.musti.rm@gmail.com>
2018-01-11 14:43:19 -0500

    started creating the js stuff

--- dom-events.js
+++ dom-events.js
@@ -1,19 +1,19 @@
-const greetingEl = document.getElementById("greeting")
-const astrosEl = document.getElementById("astros")
+// const greetingEl = document.getElementById("greeting")
+// const astrosEl = document.getElementById("astros")
+//
+// window.onload = () => {
+//   greetingEl.innerText = "oh, hey there!"
+  // renderAstros()
+// }
 
-window.onload = () => {
-  greetingEl.innerText = "oh, hey there!"
-  renderAstros()
-}
-
-renderAstros = () => {
-  const astros = peopleInSpace["people"]
-  let peopleEls = astros.map((a) => {
-    let name = a["name"]
-    return `<li>${name}</li>`
-  })
-  astrosEl.innerHTML = `<ul>${peopleEls.join("")}</ul>`
-}
+// renderAstros = () => {
+//   const astros = peopleInSpace["people"]
+//   let peopleEls = astros.map((a) => {
+//     let name = a["name"]
+//     return `<li>${name}</li>`
+//   })
+//   astrosEl.innerHTML = `<ul>${peopleEls.join("")}</ul>`
+// }
 
 // create a function that alerts "these are all the astronauts!" when the title element is clicked.
 
@@ -24,14 +24,42 @@ title.onclick = () => {
 
  // log to the console the client's x coordinate of their mouse as they move it around the window.
 
+function getPos(e) {
+  x = e.clientX
+  // console.log(x)
+  document.getElementById("mouseposition").innerHTML=x
+}
+
+
 // make an element that displays the most recent key pressed in the DOM
+
+function uniCharCode(event) {
+    var char = event.which || event.keyCode;
+    var final = String.fromCharCode(char)// console.log(char)
+    document.getElementById("mostrecent").innerHTML = final;
+}
 
 // create a text input element. When a user clicks on the text field to input, log the input element to console.
 
+function log() {
+  var text = document.getElementById('inputtext').value
+  console.log(text)
+  document.getElementById("userinput").innerHTML=text
+  event.preventDefault();
+}
+
 // when a user clicks away from the input, log ("bye") to the console.
-
 // wrap the input element with a form element.
-
 // when the form is submitted, render the client's inputted text to the DOM
+
+
+function clickaway() {
+  console.log("bye")
+  event.preventDefault();
+}
+
+
+// NO EXTRA JAVSCRIPT NEEDED HERE
+
 
 // HINT: look into event.preventDefault()
